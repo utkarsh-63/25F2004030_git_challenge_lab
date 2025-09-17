2a916464668658d26d9e6aa323351392603f7ff5
utkarsh-63 <utk63094@gmail.com>
2025-09-17 04:29:34 +0000

    Add difference module

---
+++ difference.py
@@ -0,0 +1,8 @@
+def difference_numbers(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print(f"Difference: {difference_numbers(num1, num2)}")
+
